99db088c4e9498a2dc4793c6d9e79377b105e13f
phamngocthi <pnthi.it.cp@gmail.com>
2024-03-02 18:27:07 +0700

    Hien thi danh sach cac contact lay ve tu server

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -15,5 +14,11 @@ export default defineConfig({
   },
   server: {
     port: 3001,
-  }
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000/",
+        changeOrigin: true,
+      },
+    },
+  },
 })
